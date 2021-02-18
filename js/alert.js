1083133c04fc0d25afb599e20c9eb21826cfda24
NarzulloevRR <NarzulloevRR@gmail.com>
2021-02-18 17:50:03 +0400

    fixed

--- js/alert.js
+++ js/alert.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-let modal = document.getElementById("my_modal");
- let btn = document.getElementById("btn_modal_window");
- let span = document.getElementsByClassName("close_modal_window")[0];
-
- btn.onclick = function () {
-    modal.style.display = "block";
- }
-
- span.onclick = function () {
-    modal.style.display = "none";
-=======
 const modal = document.getElementById("my_modal");
  const btn = document.getElementById("btn_modal_window");
  const span = document.getElementsByClassName("close_modal_window")[0];
@@ -20,18 +8,13 @@ const modal = document.getElementById("my_modal");
 
  span.onclick = function () {
    modal.classList.remove('modal_active');
->>>>>>> 7686529 (fixed)
  }
 
  window.onclick = function (event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+      modal.classList.remove('modal_active');
     }
-<<<<<<< HEAD
-}
-=======
 }
 
 
 
->>>>>>> 7686529 (fixed)
